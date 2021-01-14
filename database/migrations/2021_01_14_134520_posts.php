1e3ee66f7974937c17a6dd8ca5097562cb40aa89
yashacs <yashkumarchanda@gmail.com>
2021-01-14 19:42:26 +0530

    added migrations

---
+++ database/migrations/2021_01_14_134520_posts.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('featured_image');
+            $table->dateTime('deleted_at');
+            $table->string('thumbnail_image');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        DB::table('user_type')->insert([
+            'type' => 'Super Admin'
+        ]);
+        DB::table('user_type')->insert([
+            'type' => 'Editor'
+        ]);
+        DB::table('user_type')->insert([
+            'type' => 'Reader'
+        ]);
+        DB::table('users')->insert([
+            'user_type' => 1,
+            'name' => 'John',
+            'email' => 'john@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'user_type' => 2,
+            'name' => 'David',
+            'email' => 'david@gmail.com',
+            'password' => Hash::make('secret'),
+        ]);
+        DB::table('users')->insert([
+            'user_type' => 3,
+            'name' => 'Jack',
+            'email' => 'jack@gmail.com',
+            'password' => Hash::make('protected'),
+        ]);
     }
 }
